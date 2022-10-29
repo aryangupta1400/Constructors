56bdde57ee72b6817c6e47ec19fe522708e05a7a
Aryan.Gupta <Aryan.Gupta@BRD-3917L13-L>
2022-10-29 17:11:50 +0530

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// 10) Created demo for default and parameterized constructor
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Aryan", 22, "Male");
+            person.Display();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        string name;
+        int age;
+        string gender;
+        public string maritnalStatus;
+
+
+
+        public Person(string name, int age, string gender) // parameterized constructor
+        { 
+            this.name = name; 
+            this.age = age; 
+            this.gender = gender;
+            maritnalStatus = "Not Mentioned"; // default constructor 
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(" Hi! My name is " + name + ",\n Age = " + age + ",\n Gender = " + gender + ",\n Maritnal Status = " + maritnalStatus);
+        }
+    }
+}
